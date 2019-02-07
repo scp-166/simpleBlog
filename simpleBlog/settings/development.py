d4b625c39e8d7c219c50a54711a4787f4bfb87ec
scp-166 <scp-166@users.noreply.github.com>
2019-02-07 14:43:35 +0800

    添加favicon.ico

--- simpleBlog/settings/development.py
+++ simpleBlog/settings/development.py
@@ -20,9 +20,9 @@ from .base import *
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
