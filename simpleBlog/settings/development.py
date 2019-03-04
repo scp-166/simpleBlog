7f76981900443da627ba60613f4b6b1f7a21e670
scp-166 <scp-166@users.noreply.github.com>
2019-03-04 18:07:47 +0800

    修复博客列表分页显示

--- simpleBlog/settings/development.py
+++ simpleBlog/settings/development.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
